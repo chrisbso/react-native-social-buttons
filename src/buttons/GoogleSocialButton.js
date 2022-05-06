9f1fff4db52bf14460b8f8ae326eb86340f89bda
Christoffer Bro Sørensen <christofferbrosoer@outlook.com>
2022-05-06 12:44:50 +0200

    Update GoogleSocialButton.js
    Added disabled prop

--- src/buttons/GoogleSocialButton.js
+++ src/buttons/GoogleSocialButton.js
@@ -34,6 +34,7 @@ export class GoogleSocialButton extends React.Component {
       <TouchableOpacity
         style={{ ...styles.googleStyle, ...this.props.buttonViewStyle }}
         onPress={this.props.onPress}
+        disabled={this.style.disabled}
       >
         <Image
           source={require("../images/google.png")}
